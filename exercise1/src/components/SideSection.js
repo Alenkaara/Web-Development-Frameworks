a5323adbb2e1d471734336a9102caa9004cb6058
Alenkaara <78751694+Alenkaara@users.noreply.github.com>
2021-09-14 11:47:17 +0300

    Muokattu

--- exercise1/src/components/SideSection.js
+++ exercise1/src/components/SideSection.js
@@ -3,7 +3,7 @@ import styles from './SideSection.module.css';
 
 export default function SideSection(props) {
     return (
-        <div className={ styles.header }>
+        <div className={ styles.container }>
             <span className={ styles.sideSectionTitle }>{props.SideSectionTitle}  </span>
             
         </div>
